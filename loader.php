60e7f57878661af54c5667d654328100ed2a1189
shahjalal <rjshahjalal132@gmail.com>
2024-11-12 16:22:39 +0600

    initialize plugin

---
+++ loader.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Load files for plugin which are not responsible for autoloading.
+ *
+ * @link              https://github.com/mtmsujan
+ * @since             1.0.0
+ * @package           hello-again
+ */
+
+// include helper functions
+include_once PLUGIN_BASE_PATH . '/inc/helpers/helper-functions.php';
