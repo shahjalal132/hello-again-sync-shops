6e4cf3cab4d1a42fc6a6239f8447963cd80d08d0
shahjalal <rjshahjalal132@gmail.com>
2024-11-13 12:43:21 +0600

    created user info, address, social media metabox

--- inc/files/file-metabox.php
+++ inc/files/file-metabox.php
@@ -14,14 +14,114 @@ CSF::createMetabox( $prefix_page_opts, array(
 
 // Create Section
 CSF::createSection( $prefix_page_opts, array(
-    'title'  => 'Overview',
-    'icon'   => 'fas fa-rocket',
+    'title'  => 'User Info',
     'fields' => array(
         array(
-            'id'    => 'opt-text',
+            'id'    => '_email',
             'type'  => 'text',
-            'title' => 'Text',
+            'title' => 'Email',
+            'placeholder' => 'Enter email address',
+        ),
+        array(
+            'id'    => '_company',
+            'type'  => 'text',
+            'title' => 'Company',
+            'placeholder' => 'Enter company name',
+        ),
+        array(
+            'id'    => '_phone_number',
+            'type'  => 'text',
+            'title' => 'Phone Number',
+            'placeholder' => 'Enter phone number',
+        ),
+        array(
+            'id'    => '_birthday',
+            'type'  => 'date',
+            'title' => 'Birthday',
+        ),
+        array(
+            'id'      => '_gender',
+            'type'    => 'radio',
+            'title'   => 'Gender',
+            'options' => array(
+                'male'   => 'Male',
+                'female' => 'Female',
+            ),
+        ),
+        array(
+            'id'    => '_points',
+            'type'  => 'number',
+            'title' => 'Points',
+            'placeholder' => 'Enter points',
         ),
 
+    ),
+) );
+
+// Create Section
+CSF::createSection( $prefix_page_opts, array(
+    'title'  => 'Address',
+    'fields' => array(
+        array(
+            'id'    => '_street',
+            'type'  => 'text',
+            'title' => 'Street',
+            'placeholder' => 'Enter street',
+        ),
+        array(
+            'id'    => '_city_code',
+            'type'  => 'number',
+            'title' => 'City Code',
+            'placeholder' => 'City code',
+        ),
+        array(
+            'id'    => '_city',
+            'type'  => 'text',
+            'title' => 'City',
+            'placeholder' => 'Enter city',
+        ),
+        array(
+            'id'    => '_street_number',
+            'type'  => 'text',
+            'title' => 'Street Number',
+            'placeholder' => 'Enter street number',
+        ),
+        array(
+            'id'    => '_state',
+            'type'  => 'text',
+            'title' => 'Street',
+            'placeholder' => 'Enter state',
+        ),
+        array(
+            'id'    => '_country',
+            'type'  => 'text',
+            'title' => 'Country',
+            'placeholder' => 'Enter country',
+        ),
+    ),
+) );
+
+// Create Section
+CSF::createSection( $prefix_page_opts, array(
+    'title'  => 'Social Media',
+    'fields' => array(
+        array(
+            'id'    => '_fbid',
+            'type'  => 'text',
+            'title' => 'Facebook',
+            'placeholder' => 'Enter facebook id',
+        ),
+        array(
+            'id'    => '_apple_user_id',
+            'type'  => 'text',
+            'title' => 'Apple',
+            'placeholder' => 'Enter apple id',
+        ),
+        array(
+            'id'    => '_google_id',
+            'type'  => 'text',
+            'title' => 'Google',
+            'placeholder' => 'Enter google id',
+        ),
     ),
 ) );
